2d13e21640dbad120a69abf2078fcd0c84552e3a
shivaajay54 <shivaajay54@gmail.com>
2024-05-10 14:32:42 +0000

    files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,73 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
 /*=============== SWIPER CLOTHING ===============*/
+let swiperHome = new Swiper('.home__swiper', {
 
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        768: {
+            slidesPerView: 3,
+            centeredSlides: 'auto',
+        },
+        1152: {
+            centeredSlides: 'auto',
+            spaceBetween: -64,
+        },
+    }
+
+  });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 2500,
+    // reset: true
+})
+
+sr.reveal('.home__swiper, .home__footer')
+sr.reveal('.home__circle', {scale: 1.5, delay: 300})
+sr.reveal('.home__subcircle', {scale: 1.5, delay: 500})
+sr.reveal('.home__title', {scale: 1, origin: 'bottom', delay: 500})
+
+sr.reveal('.swiper-button-next, .swiper-button-prev', {origin: 'bottom'})
